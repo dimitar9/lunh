4b012e442502302ba1df07daab3fddfbb7b11d1e
dimitar9 <dimitar9909@gmail.com>
2014-11-20 00:25:54 -0500

    db

--- models/db.js
+++ models/db.js
@@ -2,4 +2,4 @@ var settings = require('../settings'),
     Db = require('mongodb').Db,
     Connection = require('mongodb').Connection,
     Server = require('mongodb').Server;
-module.exports = new Db('heroku_app31786437', new Server('mongodb://heroku_app31786437:e65g3btvanua3gfbmk6s23gl5v@ds051740.mongolab.com', 51740, {}), {safe: true});
+module.exports = new Db('heroku_app31786437', new Server('mongodb://heroku_app31786437:e65g3btvanua3gfbmk6s23gl5v@ds051740.mongolab.com:51740/heroku_app31786437', 51740, {}), {safe: true});
