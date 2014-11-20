af71a7c2002abd6097e204ac48d90ce39ec2c17c
dimitar9 <dimitar9909@gmail.com>
2014-11-19 23:58:33 -0500

    setting

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
 cookieSecret: 'lunchdecider2014',
-db: 'blog',
-host: 'mongodb://heroku_app31786437:e65g3btvanua3gfbmk6s23gl5v@ds051740.mongolab.com:51740/heroku_app31786437',
+db: 'heroku_app31786437',
+host: 'mongodb://heroku_app31786437:e65g3btvanua3gfbmk6s23gl5v@ds051740.mongolab.com:51740/'
 };
