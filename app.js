ca6ef54e67763d2bb621ba2e14a625dd7ceaf0ea
dimitar9 <dimitar9909@gmail.com>
2014-11-20 22:51:04 -0500

    db

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(flash());
 var mongodb_options = {
     db: 'heroku_app31786437',
     host: 'mongodb://heroku_app31786437:e65g3btvanua3gfbmk6s23gl5v@ds051740.mongolab.com:51740',
-    //port: 51740,
+    port: '',
     stringify: true,
     collection: 'sessions',
     auto_reconnect: false,
